dcb5718445d2fe3e976dfb04456e13c6fdc7d3e3
AndreasArnholm <andreas@arnholm.org>
2021-04-19 12:22:42 +0200

    changes in camera node

--- ros2/kmr_communication/kmr_communication/nodes/camera_node.py
+++ ros2/kmr_communication/kmr_communication/nodes/camera_node.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
+from os import error
 import sys
 import rclpy
 import argparse
 from std_msgs.msg import String, Float64
 from rclpy.node import Node
 from rclpy.utilities import remove_ros_args
-import os
+import subprocess
 
 def cl_red(msge): return '\033[31m' + msge + '\033[0m'
 def cl_green(msge): return '\033[32m' + msge + '\033[0m'
@@ -20,12 +21,21 @@ class CameraNode(Node):
         self.id = self.get_parameter('id').value
         self.declare_parameter('udp/ip')
         self.ip = self.get_parameter('udp/ip').value
+        self.proc = None
 
-        sub_start_camera = self.create_subscription(String, 'start_camera', self.start_camera, 10)
+        sub_camera = self.create_subscription(String, 'handle_camera', self.handle_camera, 10)
 
-    def start_camera(self, data):
-        print("Starting camera")
-        os.system("bash kmr_communication/kmr_communication/script/startcamera.sh " + self.ip)
+    def handle_camera(self, data):
+        if data.data.lower() == "start":
+            print(cl_green("Starting camera"))
+            self.proc = subprocess.Popen(["/bin/bash", "kmr_communication/kmr_communication/script/startcamera.sh", self.ip])
+
+        elif data.data.lower() == "stop":
+            try:
+                self.proc.terminate()
+                print(cl_green("Stopping camera"))  
+            except AttributeError:
+                print(cl_red("Camera was never started, therefore never stopped"))  
     
     def tear_down(self):
         try:
--- ros2/kmr_communication/kmr_communication/script/startcamera.sh
+++ ros2/kmr_communication/kmr_communication/script/startcamera.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 udp_ip=$1
-
 while true; do raspivid -a 12 -t 0 -w 640 -h 480 -hf -ih -fps 30 -o udp://$udp_ip -cd H264 -n; sleep 5; done
