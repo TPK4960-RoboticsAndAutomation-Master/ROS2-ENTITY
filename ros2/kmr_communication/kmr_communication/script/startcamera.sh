2f43b5b570267d75cde433687da4ab104665666a
AndreasArnholm <andreas@arnholm.org>
2021-04-22 14:11:08 +0200

    changes to raspivid

--- ros2/kmr_communication/kmr_communication/script/startcamera.sh
+++ ros2/kmr_communication/kmr_communication/script/startcamera.sh
@@ -2,4 +2,4 @@
 udp_ip=$1
 #while true; do raspivid -a 12 -t 0 -w 640 -h 480 -hf -ih -fps 30 -o udp://$udp_ip -cd H264 -n; sleep 5; done
 
-while true; do raspivid -a 12 -a "ras2" -t 0 -b 3000000 -fps 24 -w 640 -h 480 -o udp://$udp_ip -ih -fl -stm -if adaptive -pf main -v -n; sleep 1; done
+while true; do raspivid -a 12 -a "ras2" -t 0 -b 3000000 -fps 60 -w 1280 -h 720 -o udp://$udp_ip -ih -fl -stm -if adaptive -pf main -n; sleep 2; done
