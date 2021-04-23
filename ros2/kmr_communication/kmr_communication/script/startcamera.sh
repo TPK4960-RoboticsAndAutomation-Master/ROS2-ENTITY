55e00a1241cbbbd1eb124d5edda199834b1761e3
AndreasArnholm <andreas@arnholm.org>
2021-04-23 12:20:16 +0200

    raspivid speed paramater removed

--- ros2/kmr_communication/kmr_communication/script/startcamera.sh
+++ ros2/kmr_communication/kmr_communication/script/startcamera.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 udp_ip=$1
-while true; do raspivid -a 12 -t 0 -b 3000000 --speed 2 -fps 24 -w 640 -h 480 -o udp://$udp_ip -ih -fl -stm -if adaptive -pf main -n; sleep 1; done
+while true; do raspivid -a 12 -t 0 -b 3000000 -fps 24 -w 640 -h 480 -o udp://$udp_ip -ih -fl -stm -if adaptive -pf main -n; sleep 1; done
