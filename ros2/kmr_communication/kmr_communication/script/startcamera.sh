f7348b669165273c025cf9d856d09846d51dabb3
AndreasArnholm <andreas@arnholm.org>
2021-04-23 12:08:42 +0200

    raspivid speed paramater added

--- ros2/kmr_communication/kmr_communication/script/startcamera.sh
+++ ros2/kmr_communication/kmr_communication/script/startcamera.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 udp_ip=$1
-#while true; do raspivid -a 12 -t 0 -w 640 -h 480 -hf -ih -fps 30 -o udp://$udp_ip -cd H264 -n; sleep 5; done
-#while true; do raspivid -a 12 -t 0 -b 1500000 -fps 24 -w 640 -h 480 -o udp://$udp_ip -ih -fl -if adaptive -pf baseline -n; sleep 1; done
-
-while true; do raspivid -a 12 -t 0 -b 3000000 -fps 24 -w 640 -h 480 -o udp://$udp_ip -ih -fl -stm -if adaptive -pf main -n; sleep 1; done
+while true; do raspivid -a 12 -t 0 -b 3000000 --speed 2 -fps 24 -w 640 -h 480 -o udp://$udp_ip -ih -fl -stm -if adaptive -pf main -n; sleep 1; done
