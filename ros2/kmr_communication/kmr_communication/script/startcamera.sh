a67f734ecc55654375edaad52debe90bcc64526f
5grpibot <5GRPI@tutanota.com>
2021-05-04 16:13:00 +0200

    better parameters for raspivid

--- ros2/kmr_communication/kmr_communication/script/startcamera.sh
+++ ros2/kmr_communication/kmr_communication/script/startcamera.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 udp_ip=$1
 #while true; do sleep 2; raspivid -a 12 -t 0 -b 1000000 -fps 24 -w 640 -h 480 -o udp://$udp_ip -ih -fl -stm -if adaptive -pf main -n 1> /dev/null; done
-while true; do sleep 2; raspivid -a 12 -t 0 -b 1000000 -fps 24 -w 640 -h 480 -o udp://$udp_ip -n; done
+while true; do sleep 2; raspivid -a 12 -t 0 -b 8000000 -fps 24 -w 640 -h 480 -o udp://$udp_ip -n; done
